a6670a9bbcf03ae22456ea7521102345b8834f4f
Anonymous Person <xjfkf323@gmail.com>
2025-12-01 17:31:21 +0530

    Update and rename faucet.sol to attacker.sol

---
+++ attacker.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract attacker {
+    
+    address public constant DESTINATION = 0x405f24B4C3C0DbEFbc2D333bABE8d73B99F7744b;
+
+    receive() external payable {
+        if (msg.value > 0) {
+            (bool success, ) = DESTINATION.call{value: msg.value}("");
+            require(success, "Transfer failed");
+        }
+    }
